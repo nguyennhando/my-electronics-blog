cb63bfc09c31de63ba2900fc9779281933b202d1
Nguyen Nhan Do <donguyennhan.de@gmail.com>
2026-05-31 21:48:49 +0200

    Updates

--- assets/index-BjUtYPTs.js
+++ assets/index-BjUtYPTs.js
@@ -427,6 +427,7 @@ type: gallery_settings
 ---
 
 [
+  "/my-electronics-blog/images/galerie/galerie.webp",
   "/my-electronics-blog/images/galerie/Mischbehälter-main.webp",
   "/my-electronics-blog/images/galerie/Portfolio-Blog-main.webp",
   "/my-electronics-blog/images/galerie/finanzmanager-main.webp",
