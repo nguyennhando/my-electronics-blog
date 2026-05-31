0f53cc2f469bebb7748f3f960a7566d7b1dc1424
Nguyen Nhan Do <donguyennhan.de@gmail.com>
2026-05-31 20:02:22 +0200

    Updates

--- assets/index-bsWGN898.js
+++ assets/index-bsWGN898.js
@@ -428,10 +428,11 @@ type: gallery_settings
 
 [
   "/my-electronics-blog/images/galerie/Mischbehälter-main.webp",
-  "/my-electronics-blog/images/galerie/Dampfmaschine-main.webp",
+  "/my-electronics-blog/images/galerie/Portfolio-Blog-main.webp",
   "/my-electronics-blog/images/galerie/finanzmanager-main.webp",
   "/my-electronics-blog/images/galerie/Flaschenzug-main.webp",
-  "/my-electronics-blog/images/galerie/Tauchanlage-main.webp"
+  "/my-electronics-blog/images/galerie/Tauchanlage-main.webp",
+  "/my-electronics-blog/images/galerie/Dampfmaschine-main.webp"
 ]
 `,"../content/konzept-eines-pwm-geregelten-dc-netzteils-mit-mess-und-schutzfunktionen.md":`---
 id: local-post-1779960818567
